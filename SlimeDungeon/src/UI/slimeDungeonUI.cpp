5ab6f3badf7f5b0076b9c64fc4bd118159de12e5
Anthony Beaudoin <abeaudoin2@hotmail.ca>
2020-01-29 09:10:32 -0500

    Mise en place GUI
    Ajout du squelette du GUI inspiré de l'exemple d'OF

---
+++ SlimeDungeon/src/UI/slimeDungeonUI.cpp
@@ -0,0 +1,132 @@
+#include "SlimeDungeonUI.h"
+
+	
+//--------------------------------------------------------------
+void SlimeDungeonUI::setup(){
+	ofSetVerticalSync(true);
+	
+	// we add this listener before setting up so the initial circle resolution is correct
+	circleResolution.addListener(this, &SlimeDungeonUI::circleResolutionChanged);
+	ringButton.addListener(this,&SlimeDungeonUI::ringButtonPressed);
+
+	gui.setup("panel"); // most of the time you don't need a name but don't forget to call setup
+	gui.add(filled.set("bFill", true));
+	gui.add(radius.set( "radius", 140, 10, 300 ));
+	gui.add(center.set("center",glm::vec2(ofGetWidth()*.5,ofGetHeight()*.5),glm::vec2(0,0),glm::vec2(ofGetWidth(),ofGetHeight())));
+	gui.add(color.set("color",ofColor(100,100,140),ofColor(0,0),ofColor(255,255)));
+	gui.add(circleResolution.set("circleRes", 5, 3, 90));
+	gui.add(twoCircles.setup("twoCircles"));
+	gui.add(ringButton.setup("ring"));
+	gui.add(screenSize.set("screenSize", ""));
+	
+	bHide = false;
+
+	ring.load("ring.wav");
+}
+
+//--------------------------------------------------------------
+void SlimeDungeonUI::exit(){
+	ringButton.removeListener(this,&SlimeDungeonUI::ringButtonPressed);
+}
+
+//--------------------------------------------------------------
+void SlimeDungeonUI::circleResolutionChanged(int & circleResolution){
+	ofSetCircleResolution(circleResolution);
+}
+
+//--------------------------------------------------------------
+void SlimeDungeonUI::ringButtonPressed(){
+	ring.play();
+}
+
+//--------------------------------------------------------------
+void SlimeDungeonUI::update(){
+}
+
+//--------------------------------------------------------------
+void SlimeDungeonUI::draw(){
+    ofBackgroundGradient(ofColor::white, ofColor::gray);
+    
+	if( filled ){
+		ofFill();
+	}else{
+		ofNoFill();
+	}
+
+	ofSetColor(color);
+	if(twoCircles){
+		ofDrawCircle(center->x-radius*.5, center->y, radius );
+		ofDrawCircle(center->x+radius*.5, center->y, radius );
+	}else{
+		ofDrawCircle((glm::vec2)center, radius );
+	}
+	
+	if( !bHide ){
+		gui.draw();
+	}
+}
+
+//--------------------------------------------------------------
+void SlimeDungeonUI::keyPressed(int key){
+	if( key == 'h' ){
+		bHide = !bHide;
+	}
+	if(key == 's') {
+		gui.saveToFile("settings.xml");
+	}
+	if(key == 'l') {
+		gui.loadFromFile("settings.xml");
+	}
+	if(key == ' '){
+		color = ofColor(255);
+	}
+}
+
+//--------------------------------------------------------------
+void SlimeDungeonUI::keyReleased(int key){
+	
+}
+
+//--------------------------------------------------------------
+void SlimeDungeonUI::mouseMoved(int x, int y ){
+	
+}
+
+//--------------------------------------------------------------
+void SlimeDungeonUI::mouseDragged(int x, int y, int button){
+}
+
+//--------------------------------------------------------------
+void SlimeDungeonUI::mousePressed(int x, int y, int button){
+	
+}
+
+//--------------------------------------------------------------
+void SlimeDungeonUI::mouseReleased(int x, int y, int button){
+	
+}
+
+//--------------------------------------------------------------
+void SlimeDungeonUI::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void SlimeDungeonUI::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void SlimeDungeonUI::windowResized(int w, int h){
+    screenSize = ofToString(w) + "x" + ofToString(h);
+}
+
+//--------------------------------------------------------------
+void SlimeDungeonUI::gotMessage(ofMessage msg){
+	
+}
+
+//--------------------------------------------------------------
+void SlimeDungeonUI::dragEvent(ofDragInfo dragInfo){ 
+	
+}
