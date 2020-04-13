2be8313839f7bcaa7de55bc346a571193efe0a5a
Anthony Beaudoin <abeaudoin2@hotmail.ca>
2020-04-13 15:29:30 -0400

    9.1 done

---
+++ SlimeDungeon/src/Domain/CatmullRomSpline.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "ofMain.h"
+#include <string>
+
+
+class Cubemap
+{
+public:
+	Cubemap(int p_boxSize = 1500);
+	void draw();
+
+private:
+	glm::mat4x3 nx, ny, nz, px, py, pz;
+	void setPositions();
+	ofTexture faces[6];
+	int m_boxSize;
+	glm::mat4x3 positions[6];
+};
+
